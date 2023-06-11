001ecd11920802a3d1cd10e34a67defb3e5f3422
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-11 12:31:08 +0300

    Connect HomeView to the db endpoint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    Home 
+    <div v-if="projects.length">
+      <div v-for="project in projects" v-bind:key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <p>Loading projects...</p>
+    </div>
   </div>
 </template>
 
@@ -8,6 +15,17 @@
 
 export default {
   name: 'HomeView',
-  components: {}
+  components: {},
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch( 'http://localhost:3000/project' )
+      .then( response => response.json() )
+      .then( data => this.projects = data )
+      .catch( err => console.log( err.message ) )
+  }
 }
 </script>
