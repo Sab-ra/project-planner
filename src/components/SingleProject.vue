db0a5345e8c63ee1b0ff2e85de09a3965d63904f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-11 14:57:46 +0300

    Toggle on/off project details

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,32 +1,44 @@
 <template>
   <div class="project">
     <div class="actions">
-      <h3>{{ project.title }}</h3>
+      <h3 @click="toggleDetails">{{ project.title }}</h3>
     </div>
-    <div class="details">
+    <div v-if="show_details" class="details">
       <p>{{ project.details }}</p>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: [
-      'project'
-    ]
+import { assertTSConstructSignatureDeclaration } from '@babel/types';
+
+export default {
+  props: [
+    'project'
+  ],
+  data() {
+    return {
+      show_details: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+    this.show_details = !this.show_details
+    }
   }
+}
 </script>
 
 <style>
-.project {
-  margin: 20px auto;
-  background: white;
-  padding: 10px 20px;
-  border-radius: 4px;
-  box-shadow: 1px 2px 3px rgba( 0, 0, 0, 0.05 );
-  border-left: 4px solid #e90074;
-}
-h3 {
-  cursor: pointer;
-}
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba( 0, 0, 0, 0.05 );
+    border-left: 4px solid #e90074;
+  }
+  h3 {
+    cursor: pointer;
+  }
 </style>
