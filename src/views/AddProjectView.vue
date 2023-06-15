1d749d8a5ecb8c939b5ee729e06c5f7ef6fde7d7
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-15 18:30:39 +0300

    Flash out input form template

---
+++ src/views/AddProjectView.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <label>Title</label>
+    <input type="text" required>
+    <label>Details:</label>
+    <textarea required></textarea>
+  </form>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
