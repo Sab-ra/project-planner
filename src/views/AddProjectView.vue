f6d8731d32952ac08dbda0545933a314fa4612de
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-15 19:44:39 +0300

    Make input feed data and submit

--- src/views/AddProjectView.vue
+++ src/views/AddProjectView.vue
@@ -1,15 +1,26 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Title</label>
-    <input type="text" required>
+    <input type="text" v-model="title" required>
     <label>Details:</label>
-    <textarea required></textarea>
+    <textarea v-model="details" required></textarea>
+    <button>Add Project</button>
   </form>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        title: '',
+        details: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        console.log( this.title, this.details )
+      }
+    }
   }
 </script>
 
