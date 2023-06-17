78ada4852d92612ae65d51570537c19c35f7e53c
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-17 19:15:58 +0300

    Style FilterNav with active class

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -54,16 +54,5 @@ export default {
 </script>
 
 <style>
-.filter-nav button {
-  background: none;
-  border: none;
-  color: #bbb;
-  outline: none;
-  font-size: 12px;
-  text-transform: uppercase;
-  margin-right: 10px;
-  letter-spacing: 1px;
-  font-weight: bold;
-  cursor: pointer;
-}
+
 </style>
