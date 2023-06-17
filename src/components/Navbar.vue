fc7525162df3d16aafaca1ff6e827c1ff00ce25f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-17 09:14:17 +0300

    Enable navigation with navbar links functional requirement - navigation between projects and add project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="main-nav">
+    <router-link v-bind:to="{ name: 'home' }">Projects</router-link>
+    <router-link v-bind:to="{ name: 'add_project' }">Add a new project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
