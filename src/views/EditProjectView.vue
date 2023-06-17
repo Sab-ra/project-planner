48cdef2958a8c4073f947aa2fc5c2dd669b5c2c5
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-06-17 15:58:54 +0300

    Update project DB and frontend

--- src/views/EditProjectView.vue
+++ src/views/EditProjectView.vue
@@ -1,10 +1,10 @@
 <template>
-    <form>
+    <form @submit.prevent="handleEdit">
     <label>Title</label>
     <input type="text" v-model="title" required>
     <label>Details:</label>
     <textarea v-model="details" required></textarea>
-    <button>Add Project</button>
+    <button>Update Project</button>
   </form>
 </template>
 
@@ -27,7 +27,22 @@ export default {
         this.title = data.title
         this.details = data.details
       })
-  } 
+  },
+  methods: {
+    handleEdit() {
+      console.log( this.title, this.details )
+      fetch( this.uri, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( { 
+          title: this.title,
+          details: this.details 
+        })
+      }).then( () => {
+        this.$router.push( '/' )
+      }).catch( err => console.log( err ))
+    }
+  }
 }
 </script>
 
